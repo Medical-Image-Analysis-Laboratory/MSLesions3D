d9ddc9b0be08ef79b328ef6ffd5841ebaacee0d7
Maxence Wynen <maxencewynen@gmail.com>
2022-10-05 14:36:58 +0200

    training with orig dataset and commit 1

--- lesions3d/datasets.py
+++ lesions3d/datasets.py
@@ -359,7 +359,7 @@ def stats_foreground(ds, show=False):
 class ExampleDataset(pl.LightningDataModule):
     def __init__(self, n_classes=1, objects="multiple", percentage=1., augmentations=None, batch_size=8,
                  num_workers=int(os.cpu_count() / 2), verbose=False, show=False, random_state=970205, cache=False,
-                 subject=None, data_dir="/home/wynen/MSLesions3D/data/artificial_dataset"):
+                 subject=None, data_dir="/home/wynen/PycharmProjects/MSLesions3D/data/artificial_dataset"):
 
         super().__init__()
 
